2552a6767cb8238b336640f1b2a53dc8162913a1
stringly <jcs3082@hotmail.com>
2017-04-20 22:33:39 -0400

    Inital commit - Partially completed "UI and Navigation" in the Web Forms Tutorial

--- WingtipToys/Site.Master.cs
+++ WingtipToys/Site.Master.cs
@@ -7,6 +7,8 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -70,6 +72,13 @@ namespace WingtipToys
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
         }
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
--- WingtipToys/Site.Master.designer.cs
+++ WingtipToys/Site.Master.designer.cs
@@ -13,6 +13,24 @@ namespace WingtipToys {
     public partial class SiteMaster {
         
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
+        /// categoryList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView categoryList;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
